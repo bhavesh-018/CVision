1419013f1a38b0fe0bfcedbbaf5a0dcca4d479fa
bhavesh <BhaveshLakhwani@gmail.com>
2026-06-10 22:13:46 +0530

    ui: added logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,22 +7,12 @@ function Navbar() {
       <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-6">
 
         <div className="flex items-center gap-3">
-          <div className="flex h-10 w-10 items-center justify-center rounded-xl bg-blue-600">
-            <FileText size={20} />
-          </div>
-
-          <div>
-            <h1 className="font-bold">
-              AI Resume Analyzer
-            </h1>
-
-            <p className="text-xs text-slate-400">
-              Career Intelligence Platform
-            </p>
-          </div>
+          <Link to="/" className="flex items-center gap-2">
+            <img src="../../public/logo.png" className="h-52 object-contain py-10" />
+          </Link>
         </div>
 
-        <nav className="hidden md:flex items-center gap-8 text-sm text-slate-300">
+        <nav className="hidden md:flex items-center gap-6 text-sm text-slate-300">
           <Link to="/" className="hover:text-white">
             Home
           </Link>
@@ -35,11 +25,6 @@ function Navbar() {
             Resume Chat
           </Link>
         </nav>
-
-        <button className="flex items-center gap-2 rounded-xl border border-slate-700 px-4 py-2 text-sm hover:border-blue-500">
-          GitHub
-        </button>
-
       </div>
     </header>
   );
